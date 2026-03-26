62f1019bd7b4b326cbc376af054cb78c2c90bbc5
Guilherme Marucchi <guilherme.marucchi@sgaseguros.com.br>
2026-03-26 13:45:13 -0300

    feat: configure GitHub Pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/shop-list',
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
